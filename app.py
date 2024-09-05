e2a89ca238159230c0b78a188f993924147469cc
melmorabti80 <152813148+melmorabti80@users.noreply.github.com>
2024-09-05 11:13:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from views.current_season import current_season_view
 from views.archived_seasons import archived_seasons_view
+from views.delete_data import delete_data_view
 from db import create_database, verify_and_create_tables, ensure_current_season
 
 # Créer la base de données si elle n'existe pas
@@ -17,10 +18,10 @@ ensure_current_season()
 with st.sidebar:
     menu = option_menu(
         "Menu de Navigation",
-        ["Saison en cours", "Saisons archivées"],
-        icons=["calendar", "archive"],  # Icônes correspondantes (utilise la librairie Bootstrap Icons)
-        menu_icon="cast",  # Icône du menu principal
-        default_index=0,  # L'option par défaut est "Saison en cours"
+        ["Saison en cours", "Saisons archivées", "Suppression de données"],
+        icons=["calendar", "archive", "trash"],  
+        menu_icon="cast",
+        default_index=0,
     )
 
 # Affichage des vues en fonction du menu sélectionné
@@ -28,3 +29,5 @@ if menu == "Saison en cours":
     current_season_view()
 elif menu == "Saisons archivées":
     archived_seasons_view()
+elif menu == "Suppression de données":
+    delete_data_view()
